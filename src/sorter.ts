930dde58242b4d8c355cf8b8725f2ee9d459fd97
Henrik Sjööh <henriiik@gmail.com>
2015-12-06 14:37:43 +0100

    extract makeRange and add tests

--- src/sorter.ts
+++ src/sorter.ts
@@ -1,5 +1,18 @@
 import * as vscode from "vscode";
 
+export function makeRange(start: vscode.Position, end: vscode.Position) {
+	if (end.character === 0) {
+		end = end.with(end.line - 1, Number.MAX_VALUE);
+	}
+
+	if (start.line !== end.line) {
+		start = start.with(undefined, 0);
+		end = end.with(undefined, Number.MAX_VALUE);
+	}
+
+	return new vscode.Range(start, end);
+}
+
 export function sort(text: string, separator: string, locale: string, sensitivity: string) {
 	let items = text.split(separator);
 
@@ -21,20 +34,11 @@ export function sorter(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdi
 
 	let start = textEditor.selection.start;
 	let end = textEditor.selection.end;
+	let range = makeRange(start, end);
 
-	if (end.character === 0) {
-		end = end.with(end.line - 1, Number.MAX_VALUE);
-	}
-
-	if (start.line !== end.line) {
-		start = start.with(undefined, 0);
-		end = end.with(undefined, Number.MAX_VALUE);
-	}
-
-	let range = new vscode.Range(start, end);
 	let text = textEditor.document.getText(range);
-
 	let separator = (start.line === end.line) ? " " : "\n";
+
 	let sortedText = sort(text, separator, locale, sensitivity);
 
 	edit.replace(range, sortedText);
--- test/sorter.test.ts
+++ test/sorter.test.ts
@@ -1,36 +1,26 @@
 import * as assert from "assert";
-
 import * as vscode from "vscode";
-import {sort} from "../src/sorter";
+import {sort, makeRange} from "../src/sorter";
 
-interface TestData {
-	text: string;
-	sorted: string;
-}
-
-let testData: TestData[] = [
-	{ text: "b c a", sorted: "a b c" }
-];
-
-suite("sort", () => {
+suite("sort()", () => {
 	let separator = " ";
 	let locale = "en";
 	let sensitivity = "variant";
 
-	test("words", () => {
+	test("should sort words", () => {
 		assert.equal("a b c", sort("c a b", separator, locale, sensitivity));
 	});
 
-	test("lines", () => {
+	test("should sort lines", () => {
 		let separator = "\n";
 		assert.equal("a\nb\nc", sort("b\nc\na", separator, locale, sensitivity));
 	});
 
-	test("reverse", () => {
+	test("should reverse sort ", () => {
 		assert.equal("c b a", sort("a b c", separator, locale, sensitivity));
 	});
 
-	test("locale", () => {
+	test("should sort by locale", () => {
 		let text = "ä b a";
 		let sortedSV = "a b ä";
 		let localseSV = "sv";
@@ -41,12 +31,32 @@ suite("sort", () => {
 		assert.equal(sortedDE, sort(text, separator, localeDE, sensitivity));
 	});
 
-	test("case", () => {
+	test("should sort by case case", () => {
 		let text = "AA aA aa Aa";
 		let sorted = "aa aA Aa AA";
 		assert.equal(sorted, sort(text, separator, locale, sensitivity));
 
 		let sensitivityIgnore = "accent";
 		assert.equal(text, sort(text, separator, locale, sensitivityIgnore));
+	});
+});
+
+suite("makeRange()", () => {
+	let start = new vscode.Position(1,1);
+	let end = new vscode.Position(2,2);
+
+	test("should expand selection", () => {
+		let range = makeRange(start, end);
+		assert.equal(range.start.character, 0);
+		assert.equal(range.start.line, start.line);
+		assert.equal(range.end.character, Number.MAX_VALUE);
+		assert.equal(range.end.line, end.line);
+	});
+
+	test("should ignore empty newline", () => {
+		let empty = end.with(2, 0);
+		let range = makeRange(start, empty);
+		assert.equal(range.end.character, Number.MAX_VALUE);
+		assert.equal(range.end.line, 1);
 	});
 });
